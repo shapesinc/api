ebec2c4f2a2c12395c0ad44bca1234a0aab8f204
Marty AI <78570936+martysl@users.noreply.github.com>
2025-05-26 22:56:15 +0200

    Create config.php
    Signed-off-by: Marty AI <78570936+martysl@users.noreply.github.com>

---
+++ examples/apps/android-image-generator/src/api/config.php
@@ -0,0 +1,11 @@
+<?php
+return [
+  'text_api' => [
+    'endpoint' => 'https://api.shapes.inc/v1',
+    'apikey' => 'yourkey'
+  ],
+  'image_api' => [
+    'endpoint' => 'https://api.shapes.inc/v1',
+    'apikey' => 'yourkey'
+  ]
+];
