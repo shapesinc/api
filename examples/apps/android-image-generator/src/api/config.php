16c3e728f3a3e9504210f9680f421ca3f6ca233e
Marty AI <78570936+martysl@users.noreply.github.com>
2025-05-27 00:36:56 +0200

    Update config.php
    Signed-off-by: Marty AI <78570936+martysl@users.noreply.github.com>

--- examples/apps/android-image-generator/src/api/config.php
+++ examples/apps/android-image-generator/src/api/config.php
@@ -2,10 +2,12 @@
 return [
   'text_api' => [
     'endpoint' => 'https://api.shapes.inc/v1',
+    'model' => 'shapesinc/promptchecker',
     'apikey' => 'yourkey'
   ],
   'image_api' => [
     'endpoint' => 'https://api.shapes.inc/v1',
+    'model' => 'shapesinc/testimagegenerator',
     'apikey' => 'yourkey'
   ]
 ];
